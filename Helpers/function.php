7f82041af96613cc57878fc4c16b9a2945409230
yaoliuyang <762301880@qq.com>
2022-01-20 16:37:57 +0800

    update

--- Helpers/function.php
+++ Helpers/function.php
@@ -11,3 +11,28 @@ if (!function_exists('encryptPhoneNumber')) {
         return substr_replace($phoneNumber, '****', 3, 4);
     }
 }
+
+if (!function_exists('httpPost')) {
+    /**
+     * 远程http请求
+     * @param $url
+     * @param $params
+     * @return bool|string
+     */
+    function httpPost($url, $params)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json;charset=utf-8'));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        // 线下环境不用开启curl证书验证, 未调通情况可尝试添加该代码
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        return $data;
+    }
+}
