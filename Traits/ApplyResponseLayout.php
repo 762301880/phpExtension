b4aaeba51f50c8029174980d874e14a0437a383e
yaoliuyang <762301880@qq.com>
2022-01-20 17:14:13 +0800

    update

---
+++ Traits/ApplyResponseLayout.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace App\Traits;
+
+
+trait ApplyResponseLayout
+{
+
+    /**
+     * 成功返回
+     * @param string $successMsg
+     * @param array $data
+     * @param int $code
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function success($successMsg = 'success', $data = [], $code = 200)
+    {
+        return response()->json(['code' => $code, 'msg' => $successMsg, 'data' => $data]);
+    }
+
+
+    /**
+     * 错误返回
+     * @param null $errorMsg
+     * @param array $data
+     * @param int $code
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function error($errorMsg = null, $data = [], $code = 5000)
+    {
+        return response()->json(['code' => $code, 'msg' => $errorMsg, 'data' => $data]);
+    }
+
+}
