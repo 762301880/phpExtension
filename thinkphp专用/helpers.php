e48bb554767417eb6967badc30d97980f43f963b
yaoliuyang <762301880@qq.com>
2022-02-23 16:10:28 +0800

    update

---
+++ thinkphp专用/helpers.php
@@ -0,0 +1,13 @@
+<?php
+
+if (!file_exists('getLastSql')) {
+    function getLastSql()
+    {
+        \think\facade\Db::listen(function($sql, $time, $explain){
+            // 记录SQL
+            echo $sql. "&nbsp &nbsp &nbsp &nbsp".' ['.$time.'s]';
+            // 查看性能分析结果
+            echo "<br><br>".$explain;
+        });
+    }
+}
