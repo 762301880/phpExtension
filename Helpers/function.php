d64f24804cf3670cc6dbeac4578391bd787ef6e6
yaoliuyang <762301880@qq.com>
2022-04-30 09:32:13 +0800

    update

--- Helpers/function.php
+++ Helpers/function.php
@@ -1,51 +1,55 @@
 <?php
 
-
+/**
+ * 加敏手机号
+ * @param int $phoneNumber 手机号码
+ */
 if (!function_exists('encryptPhoneNumber')) {
-    /**
-     * 加敏手机号
-     * @param int $phoneNumber 手机号码
-     */
     function encryptPhoneNumber($phoneNumber)
     {
         return substr_replace($phoneNumber, '****', 3, 4);
     }
 }
 
+/** 判断是否是手机号码
+ * @param $phoneNumber
+ * @return bool
+ */
 if (!function_exists('isPhoneNumber')) {
-
-    /** 判断是否是手机号码
-     * @param $phoneNumber
-     * @return bool
-     */
     function isPhoneNumber($phoneNumber)
     {
         return preg_match('/^[0-9]{7,15}$/', $phoneNumber) > 0;
     }
 }
 
-
-if (!function_exists('httpPost')) {
-    /**
-     * 远程http请求
-     * @param $url
-     * @param $params
-     * @return bool|string
-     */
-    function httpPost($url, $params)
+/**
+ * 远程http请求
+ * @param $url
+ * @param $post_data
+ * @param $data
+ * @return bool|string
+ */
+if (!function_exists('curl_request')) {
+    function curl_request($url = '', $post_data = [], $data = [])
     {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+        $cookie = !empty($data['cookie']) ? $data['cookie'] : "";
+        $ch = curl_init();//初始化curl
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);//在尝试连接时等待的秒数。设置为0，则无限等待
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json;charset=utf-8'));
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        // 线下环境不用开启curl证书验证, 未调通情况可尝试添加该代码
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-        $data = curl_exec($ch);
-        curl_close($ch);
+        curl_setopt($ch, CURLOPT_URL, $url);//需要获取的 URL 地址
+        curl_setopt($ch, CURLOPT_HEADER, 0);//设置header param:1
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);//要求结果为字符串且输出到屏幕上
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); //禁止 cURL 验证对等证书
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false); //是否检测服务器的域名与证书上的是否一致
+        if (!empty($post_data)) { # 如果提交的参数请求不为空
+            curl_setopt($ch, CURLOPT_POST, 1);//post提交方式
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);//提交的参数
+        }
+        if (!empty($cookie)) { # 如果有cookie传递cookie
+            curl_setopt($ch, CURLOPT_COOKIE, $cookie);
+        }
+        $data = curl_exec($ch);//运行curl
+        curl_close($ch); # 关闭curl请求
         return $data;
     }
 }
@@ -70,7 +74,7 @@ if (!function_exists('sql_dump')) {
             # 打印返回sql原生语句 & sql执行时间
             echo $rawSql, "\n<br /><br />\n"
                 .
-                "<strong style='color: #AA3333'>sql_run_time:</strong>  " . "<span style='font-style: italic;color: #1e7e34'>$sql_time</span>";
+                "<strong style='color: #aa3333'>sql_run_time:</strong>  " . "<span style='font-style: italic;color: #1e7e34'>$sql_time</span>";
         });
     }
 }
