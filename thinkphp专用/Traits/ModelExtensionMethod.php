83c54f25a2d6534ef9ac1eb7eea3e36078079e57
yaoliuyang <762301880@qq.com>
2022-05-31 10:54:11 +0800

    update

--- thinkphp专用/Traits/ModelExtensionMethod.php
+++ thinkphp专用/Traits/ModelExtensionMethod.php
@@ -18,4 +18,16 @@ trait ModelExtensionMethod
         }
         return new self();
     }
+
+    /**
+     * 返回所有的字段名称
+     * 缺点必须有一条记录才可以 需要排除的字段
+     * @param array $exceptField 需要排除的字段
+     * @return array
+     */
+    public static function getFieldNames($exceptField = [])
+    {
+        $selfModel = self::field($exceptField, true)->find()->toArray();
+        return empty($selfModel) ? [] : array_keys($selfModel);
+    }
 }
