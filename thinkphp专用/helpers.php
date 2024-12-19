56377be9f0137c2f02a51e2840aaef5000b51311
yaoliuyang <762301880@qq.com>
2024-12-19 10:53:55 +0800

    update

--- thinkphp专用/helpers.php
+++ thinkphp专用/helpers.php
@@ -1,5 +1,8 @@
 <?php
 
+/**
+ * 打印最后一条执行sql
+ */
 if (!function_exists('getLastSql')) {
     function getLastSql()
     {
@@ -8,6 +11,23 @@ if (!function_exists('getLastSql')) {
             echo $sql. "&nbsp &nbsp &nbsp &nbsp".' ['.$time.'s]';
             // 查看性能分析结果
             echo "<br><br>".$explain;
+        });
+    }
+}
+
+
+/**
+ * 推荐使用  打印全部的sql
+ * https://doc.thinkphp.cn/v5_1/jiantingSQL.html
+ */
+if (!function_exists('sqlPrint')) {
+    function sqlPrint()
+    {
+        \Db::listen(function($sql, $time, $explain){
+            // 记录SQL
+            echo $sql . ' [' . $time . 's]';
+            // 查看性能分析结果
+            dump($explain);
         });
     }
 }
