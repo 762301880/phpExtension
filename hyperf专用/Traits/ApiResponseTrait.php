14721428760291b4fde6750f209a3488b3826c39
yaoliuyang <762301880@qq.com>
2026-05-23 20:45:48 +0800

    change_code_push

--- hyperf专用/Traits/ApiResponseTrait.php
+++ hyperf专用/Traits/ApiResponseTrait.php
@@ -1,26 +1,41 @@
 <?php
+
 namespace App\Traits;
+
+use Hyperf\HttpServer\Contract\ResponseInterface as HttpResponse;
+use Psr\Container\ContainerInterface;
+
 
 trait ApiResponseTrait
 {
+
+
+    protected HttpResponse $response;
+
+    public function __construct(HttpResponse $response)
+    {
+        $this->response = $response;
+    }
+
     /**
      * 成功返回
      */
-    protected function success(string $msg = 'success', array $data = [], int $code = 200)
+    protected function success(string $msg = 'success', array $data = [], int $code = 0)
     {
-        return [
+        //https://hyperf.wiki/3.1/#/zh-cn/response  响应文档
+        return $this->response->json([
             'code' => $code,
-            'msg'  => $msg,
+            'msg' => $msg,
             'data' => $data,
-        ];
+        ]);
     }
 
-    protected function error(string $msg = 'error', array $data = [], int $code = 500)
+    protected function error(string $msg = 'error', int $code = 1000)
     {
-        return [
+        return $this->response->json([
             'code' => $code,
-            'msg'  => $msg,
-            'data' => $data,
-        ];
+            'msg' => $msg,
+            'data' => null
+        ]);
     }
 }
